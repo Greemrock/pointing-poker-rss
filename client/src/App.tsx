2278ee9e8f0922f8748d37f933a2b81b3f09112c
Greemrock <fessysys911@gmail.com>
2021-09-04 01:30:53 +0300

    feat: add component member

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,16 @@
 import { FC } from 'react';
 import './App.css';
+import { Member } from './components/Member/Member';
+import Img from './components/assets/img/blane.png';
 
 export const App: FC = () => {
-  return <div>1</div>;
+  return (
+    <Member
+      firstName={'David'}
+      lastName={'Blane'}
+      jobPosition={'street magic'}
+      image={Img}
+      isYou={false}
+    />
+  );
 };
