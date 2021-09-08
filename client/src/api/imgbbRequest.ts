c3aa1e4fb74252b43e2be6b936bb061cf48b34f5
Sergey Zenkovich <sapocalipsys.z@gmail.com>
2021-09-08 21:08:19 +0300

    fix: hide api key in .env file

--- client/src/api/imgbbRequest.ts
+++ client/src/api/imgbbRequest.ts
@@ -1,12 +1,11 @@
 import axios from 'axios';
-const API_KEY = '1df4cb38923edba9f813994063d84816';
 export const postImage = async (
   image: string | null | undefined
 ): Promise<string | void> => {
   if (image) {
     const imageContent = image.split(',')[1];
     const body = new FormData();
-    body.set('key', API_KEY);
+    body.set('key', process.env.REACT_APP_IMGBB || '');
     body.append('image', imageContent);
     const result = await axios({
       method: 'post',
