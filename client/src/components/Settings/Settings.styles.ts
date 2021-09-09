c34d22cb9dc7b63dc761ec65007a27b8f43c285c
unknown <sapocalipsys.z@gmail.com>
2021-09-09 17:08:05 +0300

    feat: add switcher to settings

--- client/src/components/Settings/Settings.styles.ts
+++ client/src/components/Settings/Settings.styles.ts
@@ -1,7 +1,69 @@
+import {
+  GREEN_2,
+  SPACE_MD,
+  SPACE_LG,
+  SPACE_XS,
+} from './../../Shared/cssConstants';
+import createStyles from '@material-ui/core/styles/createStyles';
+import { Theme } from '@material-ui/core/styles/createTheme';
 import makeStyles from '@material-ui/core/styles/makeStyles';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Switch from '@material-ui/core/Switch/Switch';
+import { findByLabelText } from '@testing-library/dom';
+
+export const AntSwitch = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: 37,
+      height: 20,
+      padding: 0,
+      display: 'flex',
+    },
+    switchBase: {
+      padding: 3,
+      color: theme.palette.grey[500],
+      '&$checked': {
+        transform: 'translateX(17px)',
+        color: theme.palette.common.white,
+        '& + $track': {
+          opacity: 1,
+          backgroundColor: GREEN_2,
+          borderColor: GREEN_2,
+        },
+      },
+    },
+    thumb: {
+      width: 14,
+      height: 14,
+      boxShadow: 'none',
+    },
+    track: {
+      border: `1px solid ${theme.palette.grey[500]}`,
+      borderRadius: 10,
+      opacity: 1,
+      backgroundColor: theme.palette.common.white,
+    },
+    checked: {},
+  })
+)(Switch);
 
 export const useStyles = makeStyles(() => ({
   mainSettingsBlock: {
     width: '75%',
+  },
+  accordionInnerBlock: {
+    width: '85%',
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: SPACE_MD,
+  },
+  switcherLabel: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: `${SPACE_LG} 0`,
+    '& .MuiTypography-root': {
+      margin: `0 ${SPACE_XS}`,
+    },
   },
 }));
--- client/src/components/Settings/Settings.tsx
+++ client/src/components/Settings/Settings.tsx
@@ -5,20 +5,25 @@ import {
   AccordionSummary,
   Container,
   FormControl,
+  FormControlLabel,
   InputLabel,
   MenuItem,
   Select,
   Typography,
 } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { useStyles } from './Settings.styles';
+import { useStyles, AntSwitch } from './Settings.styles';
 
 export const Settings: React.FC = () => {
   const [deck, setDeck] = useState<string>('fibonacci');
+  const [isTimer, setIsTimer] = useState(false);
   const classes = useStyles();
 
-  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+  const handleSelectChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     setDeck(event.target.value as string);
+  };
+  const handleSwitcherChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsTimer(event.target.checked);
   };
   return (
     <Container className={classes.mainSettingsBlock}>
@@ -30,29 +35,38 @@ export const Settings: React.FC = () => {
         >
           <Typography>Settings</Typography>
         </AccordionSummary>
-        <AccordionDetails>
+        <AccordionDetails className={classes.accordionInnerBlock}>
           <FormControl variant="filled">
             <InputLabel id="deckSelectInput">Deck</InputLabel>
             <Select
               labelId="deckSelect"
               id="deckSelect"
               value={deck}
-              onChange={handleChange}
+              onChange={handleSelectChange}
             >
               <MenuItem value="fibonacci">
-                Fibonacci( 0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ?, Pass )
+                Fibonacci - ( 0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ?, Pass )
               </MenuItem>
               <MenuItem value="modified fibonacci">
-                Modified Fibonacci ( 0, ½, 1, 2, 3, 5, 8, 13, 20, 40, 100, ?,
+                Modif. Fibonacci - ( 0, ½, 1, 2, 3, 5, 8, 13, 20, 40, 100, ?,
                 Pass )
               </MenuItem>
               <MenuItem value="T-shirts">
-                T-shirts ( xxs, xs, s, m, l, xl, xxl, ?, Pass )
+                T-shirts - ( xxs, xs, s, m, l, xl, xxl, ?, Pass )
               </MenuItem>
               <MenuItem value="Powers">
-                Powers of 2 ( 0, 1, 2, 4, 8, 16, 32, 64, ?, Pass )
+                Powers of 2 - ( 0, 1, 2, 4, 8, 16, 32, 64, ?, Pass )
               </MenuItem>
             </Select>
+          </FormControl>
+          <FormControl className={classes.switcherLabel}>
+            <AntSwitch
+              checked={isTimer}
+              onChange={handleSwitcherChange}
+              id="observer"
+              name="observer"
+            />
+            <Typography>Would you like a story timer?</Typography>
           </FormControl>
         </AccordionDetails>
       </Accordion>
