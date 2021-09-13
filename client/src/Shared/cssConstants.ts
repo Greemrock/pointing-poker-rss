b282e320ae4e40f13f971dfeaa766dd87e20ace9
unknown <sapocalipsys.z@gmail.com>
2021-09-13 13:38:50 +0300

    fix:  add colors from develop branch

--- client/src/Shared/cssConstants.ts
+++ client/src/Shared/cssConstants.ts
@@ -20,6 +20,8 @@ export const RED = '#FF0000';
 export const GREEN_1 = '#60DABF75';
 export const GREEN_2 = '#60DABF';
 export const GREEN_3 = '#66999B';
+export const SAND = '#FFC482';
+export const WHITE = '#FFFFFF';
 export const GRAY = '#888888';
 
 export const BORDER = `1px solid ${GRAY}`;
