09dfe803c418b688f956a07a4d7ace7f13ec8af4
unknown <sapocalipsys.z@gmail.com>
2021-09-14 17:40:13 +0300

    feat: add context menu for kicking player

---
+++ client/src/components/DeletePlayerBlock/DeletePlayerBlock.styles.ts
@@ -0,0 +1,23 @@
+import { GREEN_3, SPACE_MD, SPACE_XL } from './../../Shared/cssConstants';
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles({
+  boxBlock: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  heding: {
+    marginTop: SPACE_MD,
+    textAlign: 'center',
+    fontWeight: 700,
+  },
+  textBlock: {
+    marginTop: SPACE_MD,
+    marginBottom: SPACE_XL,
+  },
+  nameSpan: {
+    color: GREEN_3,
+    fontWeight: 700,
+  },
+});
