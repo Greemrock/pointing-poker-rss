fa7715eb36fcf678ed567916891245af3f537876
Andrey <greemrock911@gmail.com>
2021-09-14 15:41:42 +0300

    init: create component IssueDialogForm

---
+++ client/src/components/IssueDialogForm/IssueDialogForm.styled.ts
@@ -0,0 +1,11 @@
+import { SPACE_MD } from './../../Shared/cssConstants';
+import { makeStyles } from '@material-ui/core';
+
+export const useIssueDialogFormStyles = makeStyles({
+  container: {
+    margin: `${SPACE_MD} 0`,
+  },
+  title: {
+    fontWeight: 700,
+  },
+});
---
+++ client/src/components/IssueDialogForm/IssueDialogForm.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import {
+  Dialog,
+  Typography,
+  DialogContent,
+  TextField,
+  DialogActions,
+  Button,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormControl,
+  Container,
+} from '@material-ui/core';
+import { PreloaderForForm } from '../PreloaderForForm';
+import { useFormik } from 'formik';
+import { useIssueDialogFormStyles } from './IssueDialogForm.styled';
+import { Priority } from '../../Shared/enums';
+
+export const IssueDialogForm: React.FC = () => {
+  const classes = useIssueDialogFormStyles();
+  const [isLoading, setIsLoading] = useState(false);
+  const [open, setOpen] = useState(true);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      link: '',
+      position: 'low',
+    },
+    onSubmit: (values) => {
+      setIsLoading(true);
+      const payloadObject = {
+        ...values,
+      };
+    },
+  });
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <Container className={classes.container}>
+        <Typography
+          className={classes.title}
+          component="h3"
+          variant="h3"
+          align="center"
+        >
+          Create Issue
+        </Typography>
+        {isLoading ? (
+          <PreloaderForForm />
+        ) : (
+          <form autoComplete="off" onSubmit={formik.handleSubmit}>
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="title"
+                label="Title"
+                type="text"
+                variant="outlined"
+                fullWidth
+                value={formik.values.title}
+                onChange={formik.handleChange}
+                error={formik.touched.title && Boolean(formik.errors.title)}
+                helperText={formik.touched.title && formik.errors.title}
+              />
+              <TextField
+                margin="dense"
+                id="link"
+                label="Link"
+                type="text"
+                variant="outlined"
+                fullWidth
+                value={formik.values.link}
+                onChange={formik.handleChange}
+                error={formik.touched.link && Boolean(formik.errors.link)}
+                helperText={formik.touched.link && formik.errors.link}
+              />
+              <FormControl margin="normal" fullWidth>
+                <InputLabel id="priority-label">Priority</InputLabel>
+                <Select
+                  labelId="priority-label"
+                  defaultValue={Priority.low}
+                  id="priority"
+                >
+                  <MenuItem value={Priority.low}>{Priority.low}</MenuItem>
+                  <MenuItem value={Priority.middle}>{Priority.middle}</MenuItem>
+                  <MenuItem value={Priority.hight}>{Priority.hight}</MenuItem>
+                </Select>
+              </FormControl>
+            </DialogContent>
+            <DialogActions>
+              <Button color="primary" variant="contained" type="submit">
+                Create
+              </Button>
+              <Button
+                onClick={handleClose}
+                color="secondary"
+                variant="contained"
+              >
+                Cancel
+              </Button>
+            </DialogActions>
+          </form>
+        )}
+      </Container>
+    </Dialog>
+  );
+};
