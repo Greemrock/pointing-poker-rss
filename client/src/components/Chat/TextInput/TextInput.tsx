df83d0c2542637273ba2c1a16587a1a70ebcd9f9
Andrey <greemrock911@gmail.com>
2021-09-22 01:35:33 +0300

    fix: removed not existing style

--- client/src/components/Chat/TextInput/TextInput.tsx
+++ client/src/components/Chat/TextInput/TextInput.tsx
@@ -56,7 +56,6 @@ export const TextInput: React.FC = () => {
             variant="contained"
             color="primary"
             disabled={isSubmitting}
-            className={classes.button}
           >
             <SendIcon />
           </Button>
