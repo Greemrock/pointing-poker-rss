0f699e435f0201c48a0a8e471b1755c7124d9d4f
Andrey <greemrock911@gmail.com>
2021-09-29 19:11:00 +0300

    feat: add cards to settings

--- client/src/components/CardContainer/CardContainer.tsx
+++ client/src/components/CardContainer/CardContainer.tsx
@@ -1,32 +1,28 @@
-import { Button } from '@material-ui/core';
 import React, { useState } from 'react';
-import { cardsArrays } from '../../Shared';
+import { cardsArrays, Decks } from '../../Shared';
 import { Card } from '../Card';
 import { useCardContainerStyled } from './CardContainer.styles';
 
 type Props = {
   cardSelected: boolean;
+  deck: Decks;
 };
 
-export const CardContainer: React.FC<Props> = ({ cardSelected }) => {
+export const CardContainer: React.FC<Props> = ({ cardSelected, deck }) => {
   const [isCardSelected, setCardSelected] = useState(cardSelected);
 
   const classes = useCardContainerStyled();
 
   return (
-    <>
-      <div className={classes.root}>
-        {cardsArrays.modifiedFibonacci.map((elem) => (
-          <Card
-            key={elem}
-            nameCard={elem}
-            isCardSelected={isCardSelected}
-            setCardSelected={setCardSelected}
-          />
-        ))}
-      </div>
-      <Button onClick={() => setCardSelected(false)}>Reset Select</Button>
-      <Button onClick={() => setCardSelected(true)}>Dont click</Button>
-    </>
+    <div className={classes.root}>
+      {cardsArrays[deck].map((elem) => (
+        <Card
+          key={elem}
+          nameCard={elem}
+          isCardSelected={isCardSelected}
+          setCardSelected={setCardSelected}
+        />
+      ))}
+    </div>
   );
 };
